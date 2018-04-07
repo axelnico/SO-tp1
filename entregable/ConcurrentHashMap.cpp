34b25a69aa8fe5aa102af1737a5c3ee8df9f3467
axelnico <nicobuk@gmail.com>
2018-04-07 20:06:35 -0300

    ejercicios varios

--- entregable/ConcurrentHashMap.cpp
+++ entregable/ConcurrentHashMap.cpp
@@ -32,3 +32,34 @@ void ConcurrentHashMap::addAndInc(string key){
 		}
 	}
 }
+
+/**************************************** memeber *********************************************/
+bool ConcurrentHashMap::member(string key){
+	for (auto it = tabla[hash(key)]->CrearIt(); it.HaySiguiente(); it.Avanzar()) {
+		if (it.Siguiente().first == key) return true;
+	}
+	return false;
+}
+
+/************************************ maximum *************************************************/
+pair<string,unsigned int> ConcurrentHashMap::maximum(unsigned int nt){
+	atomic<int> siguiente(0);
+	atomic<Elem *> maximo(nullptr);
+	pthread_t threads[nt]; int tid;
+	infoThread vars[nt];
+	/* Pedimos los mutex de cada entrada de la tabla */
+	for (size_t i = 0; i < 26; i++) pthread_mutex_lock(& _locks[i]);
+	/* Inizializamos los threads */
+	for (size_t tid = 0; tid < nt; tid++) {
+		vars[tid].siguiente = &siguiente;
+		vars[tid].max = &maximo;
+		vars[tid].context = this;
+		pthread_create(&threads[tid],NULL,&ConcurrentHashMap::maxThrWrapper,& (vars[tid]) );
+	}
+	/* Joineamos los threads */
+	for (size_t tid = 0; tid < nt; tid++) pthread_join(threads[tid],NULL);
+	pair<string,unsigned int> res = make_pair(maximo.load()->first,maximo.load()->second);
+	/* Desbloqueamos los mutex de cada entrada de la tabla */
+	for (size_t i = 0; i < 26; i++) pthread_mutex_unlock(& _locks[i]);
+	return res;
+}
