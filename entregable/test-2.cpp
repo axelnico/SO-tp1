fa46fa9e299485c52e04ad60a28fc8bf8601eede
Kevin <kevinfra94@gmail.com>
2018-04-10 00:22:21 -0300

    Arreglo tests

--- entregable/test-2.cpp
+++ entregable/test-2.cpp
@@ -7,7 +7,8 @@ int main(void) {
 	ConcurrentHashMap h;
 	int i;
 	
-	h = ConcurrentHashMap::count_words("corpus");
+	h = count_words("corpus");
+	cout << "cree el dicc" << endl;
 	for (i = 0; i < 26; i++) {
 		for (auto it = h.tabla[i]->CrearIt(); it.HaySiguiente(); it.Avanzar()) {
 			auto t = it.Siguiente();
