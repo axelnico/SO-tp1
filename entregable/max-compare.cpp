31f14b71ee0979f72929132fe07731e8d168df12
Kevin <kevinfra94@gmail.com>
2018-04-15 21:50:51 -0300

    Cambio forma de medir tiempos para contar ciclos de procesador del proceso y arreglo el graficador

--- entregable/max-compare.cpp
+++ entregable/max-compare.cpp
@@ -6,28 +6,57 @@
 #define BILLION  1000000000L;
 #define mil 1000
 
+#define MEDIR_TIEMPO(var)                                   \
+{                                                           \
+    __asm__ __volatile__ (                                  \
+        "xor %%rdx, %%rdx\n\t"                              \
+        "xor %%rax, %%rax\n\t"                              \
+        "lfence\n\t"                                        \
+        "rdtsc\n\t"                                         \
+        "sal $32, %%rdx\n\t"                                \
+        "or %%rdx, %%rax\n\t"                               \
+        "movq %%rax, %0\n\t"                                \
+        : "=r" (var)                                        \
+        : /* no input */                                    \
+        : "%rax", "%rdx"                                    \
+    );                                                      \
+}
+
+#define MEDIR_TIEMPO_START(start)                           \
+{                                                           \
+    /* warm up ... */                                       \
+    MEDIR_TIEMPO(start);                                    \
+    MEDIR_TIEMPO(start);                                    \
+    MEDIR_TIEMPO(start);                                    \
+}
+
+#define MEDIR_TIEMPO_STOP(end)                              \
+{                                                           \
+    MEDIR_TIEMPO(end);                                      \
+}
+
 using namespace std;
 
 int main(int argc, char **argv) {
     pair<string, unsigned int> p, threaded_p;
     list<string> l = { "corpus-0", "corpus-1", "corpus-2", "corpus-3", "corpus-4" };
-    struct timespec start1, stop1, start2, stop2;
+    unsigned long start1, stop1, start2, stop2;
 
     if (argc != 3) {
         cerr << "uso: " << argv[0] << " #tarchivos #tmaximum" << endl;
         return 1;
     }
-    clock_gettime( CLOCK_REALTIME, &start1);
+    MEDIR_TIEMPO_START(start1);
     p = maximum(atoi(argv[1]), atoi(argv[2]), l);
-    clock_gettime( CLOCK_REALTIME, &stop1);
+    MEDIR_TIEMPO_STOP(stop1);
 
-    clock_gettime( CLOCK_REALTIME, &start2);
+    MEDIR_TIEMPO_START(start2);
     threaded_p = maximum_concurrent(atoi(argv[1]), atoi(argv[2]), l);
-    clock_gettime( CLOCK_REALTIME, &stop2);
+    MEDIR_TIEMPO_STOP(stop2);
 
     if (p == threaded_p) {
-        cout << ( stop1.tv_nsec - start1.tv_nsec ) / mil;
-        cout << " " << ( stop2.tv_nsec - start2.tv_nsec ) / mil << endl;
+        cout << ( stop1 - start1 );
+        cout << " " << ( stop2 - start2 ) << endl;
     }
 
 
---
+++ scripts/graficar-1-thread.py
@@ -0,0 +1,29 @@
+import math
+import numpy as np
+import matplotlib.pyplot as plt
+import pylab
+import sys
+
+arr = np.genfromtxt("single")
+sinThread = [row[0] for row in arr]
+conThread = [row[1] for row in arr]
+
+sinThreadsAvg = np.mean(sinThread)
+conThreadsAvg = np.mean(conThread)
+bars = [sinThreadsAvg, conThreadsAvg]
+
+# arrr = np.genfromtxt("multi")
+
+# w = [row[0] for row in arrr]
+# z = [row[1] for row in arrr]
+
+x = np.arange(2)
+
+fig, ax1 = plt.subplots()
+plt.bar(x, bars)
+ax1.set_xticks(x)
+ax1.set_xticklabels(['Sin threads', 'Con 1 thread'])
+
+ax1.set_ylabel('Cantidad de ciclos de Clock')
+
+plt.show()
